f38745dad64be542163e19d5437d91d40c2d280e
tkdlrs <30321118+tkdlrs@users.noreply.github.com>
2019-01-16 12:00:40 -0700

    Added the original time that I worked the constonant please kata

--- codewars/javascript/7kyu/consonant-please.js
+++ codewars/javascript/7kyu/consonant-please.js
@@ -1,5 +1,7 @@
 // Original kata: https://www.codewars.com/kata/59727e04f6a02d0cde00001a
 
+
+// most recent time I reworked the kata
 function sortLetters(input) {
 
 var consonant = [], vowels = [], numbers = [];
@@ -19,4 +21,39 @@ var cleanedInput = input.join('').toUpperCase().split(',').join('').split('');
 
 return [vowels, consonant];
 
+}
+
+// first time completing the kata
+function sortLetters(input) {
+var numbersArray = [];
+var lettersArray = []
+// remove all numbers in both arrays by cycling through both and if it's an integer store it in numbersArray.
+for(var i = 0; i < input.length; i ++) {
+var currentArray = input[i]
+  for(var j = 0; j < currentArray.length; j++) {
+    var currentItem = currentArray[j];
+        if(Number.parseInt(currentItem) === currentItem) {
+          numbersArray.push(currentItem);
+        } else {
+          lettersArray.push(currentItem);
+        }
+  }
+}
+
+// Capitalize everything
+var capLetters = lettersArray.join("").toUpperCase() ;
+// find the vowels
+var vowels = capLetters.match(/([AEIOU])/gi);
+// find the consonants
+var consonants = capLetters.match(/([BCDFGHJKLMNPQRSTVWXYZ])/gi);
+// if no vowels return empty array.
+if(vowels == null ){
+vowels = [];
+}
+// if no consonants return empty array.
+if(consonants == null) {
+consonants = [];
+}
+// return solution.
+return [vowels, consonants];
 }
